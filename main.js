b456a851d3ae9d16469ba4884ddb1cbfb651125e
Nejc Jezeršek <njezersek@gmail.com>
2018-06-02 19:39:20 +0300

    testiranje hitrosti z več workerji in enim workerjem

--- main.js
+++ main.js
@@ -1,15 +1,46 @@
-function addWorker(){
+function parallel(){
 	if (!window.Worker)return "Worker is not suported";
-	var w = new Worker("worker.js"); //Create a new worker
+	var w1 = new Worker("worker.js"); //Create a new worker
+	var w2 = new Worker("worker.js"); //Create a new worker
 
-	w.postMessage(10000000); // Sending message as an array to the worker
-	console.log("10000000 ->");
-	w.postMessage(1);
-	console.log("1 ->");
+	console.time("test");
 
-	w.onmessage = function(e) { // Getting response from worker
-		console.log("MAIN: Worker je odgovoril: \"" + e.data + "\"");
+	let konec = false;
+
+	w1.postMessage(10000000); // Sending message as an array to the worker
+	console.log("10000000 -> w1");
+	w2.postMessage(10000000);
+	console.log("10000000 -> w2");
+
+	w1.onmessage = function(e) { // Getting response from worker
+		console.log("MAIN: Worker 1 je odgovoril: \"" + e.data + "\"");
+		if(konec)console.timeEnd("test");
+		konec = true;
+	};
+
+	w2.onmessage = function(e) {
+		console.log("MAIN: Worker 2 je odgovoril: \"" + e.data + "\"");
+		if(konec)console.timeEnd("test");
+		konec = true;
 	};
 }
 
-addWorker();
+function procedural(){
+	if (!window.Worker)return "Worker is not suported";
+	var w1 = new Worker("worker.js"); //Create a new worker
+
+	console.time("test");
+
+	let konec = false;
+
+	w1.postMessage(10000000); // Sending message as an array to the worker
+	console.log("10000000 -> w1");
+	w1.postMessage(10000000);
+	console.log("10000000 -> w1");
+
+	w1.onmessage = function(e) { // Getting response from worker
+		console.log("MAIN: Worker 1 je odgovoril: \"" + e.data + "\"");
+		if(konec)console.timeEnd("test");
+		konec = true;
+	};
+}
