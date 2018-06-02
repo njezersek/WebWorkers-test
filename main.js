4fad96d52d077887208c9c9f4a12149f7e4f25e0
Nejc Jezeršek <njezersek@gmail.com>
2018-06-02 16:14:54 +0300

    test

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 if (window.Worker) { // Check if Browser supports the Worker api.
 	var myWorker = new Worker("worker.js"); //Create a new worker
 
-	myWorker.postMessage([first.value,second.value]); // Sending message as an array to the worker
+	myWorker.postMessage([23,12]); // Sending message as an array to the worker
 
 	myWorker.onmessage = function(e) { // Getting response from worker
 		console.log(e.data);
